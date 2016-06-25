3bea6727e23ebf4d70b4fb925dcfc9fb8953976d
lili <2279545050@qq.com>
2016-06-25 15:34:36 +0800

    Initial Commit

---
+++ 纯代码-弹框/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  纯代码-弹框
+//
+//  Created by qiulili on 25/6/16.
+//  Copyright © 2016年 qiulili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 纯代码-弹框/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  纯代码-弹框
+//
+//  Created by qiulili on 25/6/16.
+//  Copyright © 2016年 qiulili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
